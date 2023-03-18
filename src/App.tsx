72faa07bd9a528fafbf2fc48495835aa74333c73
Arkadiy <alibullterrier@gmail.com>
2023-03-18 08:32:15 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,55 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type TodosType = {
+    userId: number,
+    id: number,
+    title: string,
+    completed: boolean
+}
+
 function App() {
-  return (
-    <div className="App">
-    const[todos, setTodos] = useState([])
-    </div>
-  );
+    const [todos, setTodos] = useState<TodosType[]>([])
+
+    const fetchFoo = () => {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(response => response.json())
+            .then(json => setTodos(json))
+    }
+    
+    useEffect(() => {
+        fetchFoo()
+    }, [])
+
+    const showMeTodos = () => {
+        fetchFoo()
+    }
+
+    const deleteTodos = () => {
+        setTodos([])
+    }
+  
+    return (
+        <div className="App">
+            <button onClick={showMeTodos}>Show me...</button>
+            <button onClick={deleteTodos}>DeleteTodos</button>
+            <ul>
+                {todos.map((el) => {
+                    return (
+                        <li>
+                            <span>{el.id}</span>
+                            <input type={'checkbox'} checked={el.completed}/>
+                            <span>{el.title}</span>
+
+                        </li>
+                    )
+                })}
+
+
+            </ul>
+        </div>
+    );
 }
 
 export default App;
